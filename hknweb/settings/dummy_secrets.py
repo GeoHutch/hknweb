93c216528870c48641a95b19474a0887d444ca73
George Higgins Hutchinson <hutch@hkn.eecs.berkeley.edu>
2018-06-01 23:30:57 -0700

    Added credentials for Google OAuth2

--- hknweb/settings/dummy_secrets.py
+++ hknweb/settings/dummy_secrets.py
@@ -1,4 +1,7 @@
+REAL_SECRETS = False
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'v9lj^szduvr@a*31&r(l5ub+5q%ebszts70vlpzaiekt23s)gb'
 
+GOOGLE_OAUTH2_SECRETS_JSON = 'dummy_oauth_secrets.json'
 
